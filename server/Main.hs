e032fc19e93bba220ac05852f2432f8c11038f2d
Andy Gill <andygill@ku.edu>
2016-04-29 10:49:32 -0500

    Serving an aframe

--- server/Main.hs
+++ server/Main.hs
@@ -1,7 +1,26 @@
+{-# LANGUAGE KindSignatures, GADTs, LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import qualified Control.Object as O
+import           Control.Object ((#))
+
+import Text.AFrame
 import Web.AFrame
 
+main :: IO ()
 main = do
-  aframeServer 3947 (error "")
+  x <- readFile "example.aframe"
+  case readAFrame x of
+    Nothing -> error "can not read sample file"
+    Just a -> main2 a
+    
+main2 :: AFrame -> IO ()
+main2 a = do
+  print a
+  putStrLn $ showAFrame a
+  aframeServer "/scene" 3947 $ O.Object $ \ case
+    GetAFrame   -> return $ a
+    SetAFrame _ -> return ()
 
--- src/Web/AFrame.hs
+++ src/Web/AFrame.hs
@@ -6,12 +6,18 @@ module Web.AFrame where
 import qualified Control.Object as O
 import           Control.Object ((#))
 
-import           Text.AFrame
+import           Text.AFrame as AFrame
 import qualified Web.Scotty.CRUD as W
 
 --import Network.HTTP.Types
 import Web.Scotty as S
 import Network.Wai.Middleware.Static
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.ByteString.Lazy.UTF8 as UTF8
+import           Control.Monad.IO.Class (liftIO)
+import Text.XML.Light as XML
+import qualified Data.Text.Lazy as LT
+
 
 data AFrameP :: * -> * where
   SetAFrame :: AFrame -> AFrameP ()
@@ -23,8 +29,8 @@ data AFrameP :: * -> * where
 -- This entry point generates a server that handles the AFrame.
 -- It never terminates, but can be started in a seperate thread.
 
-aframeServer :: Int -> O.Object AFrameP -> IO ()
-aframeServer port aframe = do
+aframeServer :: String -> Int -> O.Object AFrameP -> IO ()
+aframeServer scene port aframe = do
 
   let crudObj :: O.Object W.CRUD
       crudObj = O.Object $ \ case 
@@ -37,8 +43,41 @@ aframeServer port aframe = do
           W.Delete {} -> fail "Delete not supported"
 
 
+  let xRequest = do
+        S.addHeader "Access-Control-Allow-Headers" "Authorization, Origin, X-Requested-With, Content-Type, Accep"
+        S.addHeader "Access-Control-Allow-Methods" "POST, GET, PUT, DELETE, OPTIONS"
+        S.addHeader "Access-Control-Allow-Origin"  "*"
+
+
+--      ppAFrame :: AFrame -> Text
+--      ppAFrame 
+
+      xml :: String -> ActionM ()
+      xml t = do
+         S.setHeader "Content-Type" "text/html; charset=utf-8"
+         raw $ UTF8.fromString $ t
+
+      aframeToText :: AFrame -> LT.Text
+      aframeToText = LT.pack . showAFrame
+      
+      ppConfig :: XML.ConfigPP
+      ppConfig = defaultConfigPP 
+
+
+
   S.scotty port $ do
     S.middleware $ staticPolicy noDots
+
+
+    S.get (capture scene) $ do
+          xRequest
+          s <- liftIO $ do
+                  aframe # GetAFrame
+          S.html $ aframeToText $ s
+
+
+
+
 --    S.middleware logStdoutDev
 
     W.scottyCRUD "/scene" crudObj
