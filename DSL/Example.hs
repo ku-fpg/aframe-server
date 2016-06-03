0aa67f73d9b331c72e5f715bfa3c5454d2ea5566
Andy Gill <andygill@ku.edu>
2016-06-03 16:28:36 -0500

    Example with bouncing ball

--- DSL/Example.hs
+++ DSL/Example.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE  RankNTypes, FlexibleInstances, UndecidableInstances #-}
+
 module Main where
 
+import Data.Text (Text)
 import Text.AFrame
 import Text.AFrame.DSL 
 import Web.AFrame.GHCi
+import Control.Lens
 
 example :: AFrame
 example = scene $ do
@@ -10,14 +14,24 @@ example = scene $ do
     position (0,1.25,-1)
     radius   1.25
     color    "#EF2D5E"
+    animation $ do
+        attribute "id" ("animation-1" :: Text)
+        fromTo position
+              (0,1.25,-1)
+              (0,1.00,-1)
+        direction "alternate"
+        easing "linear"
+        dur 1000
+        repeat_ "indefinite"
   box $ do
+    attribute "id" ("box" :: Text)
     position (-1,0.5,1)
     rotation (0,45,0)
     width    1
     height   1
     color    "#4CC3D9"
   cylinder $ do
-    position (1,0.75,1)
+    position (1,0.75,1) 
     radius   0.5
     height   1.5
     color    "#FFC65D"
