47c0ad083408d9f9c8f3883d1b77aa66b65770c9
Andy Gill <andygill@ku.edu>
2016-06-13 16:59:29 -0500

    folders, color and numbers

--- examples/js/aframe-my-test.js
+++ examples/js/aframe-my-test.js
@@ -1,6 +1,42 @@
 console.log("my-test");
 var datGUI;
 
+function findParentGUI(el) {
+  // If the immeduate parent is a folder, use the folder.
+  if (el.parentEl && 
+    el.parentEl.components &&
+    el.parentEl.components['selection-folder']) {
+    return el.parentEl.components['selection-folder'].data.folder
+  } else {
+    if (datGUI == undefined) {
+       datGUI = new dat.GUI();       
+    }
+    return datGUI;
+  }
+}
+
+AFRAME.registerComponent('selection-folder', {
+   schema: {
+     name: { default: 'folder', type: 'string' },
+     open: { default: true, type: 'boolean' }
+   },
+   init: function () {
+     console.log('selection-folder',datGUI);
+     var g = findParentGUI(this.el);
+     var name = this.data.name;
+     var count = 2;
+     while(g.__folders[name] !== undefined) {
+         name = this.data.name + " (" + count++ + ")"
+     }
+
+     this.data.folder = g.addFolder(name);
+
+     if (this.data.open) {
+       this.data.folder.open();
+     }
+   }
+});
+
 AFRAME.registerComponent('color-selector', {
    schema: {
      color: { default: '#FFF', type: 'string' },
@@ -11,30 +47,36 @@ AFRAME.registerComponent('color-selector', {
      if (datGUI == undefined) {
         datGUI = new dat.GUI();       
      }
-     console.log(this.el.parentEl.components)
-     var g = datGUI;
-     if (this.el.parentEl && 
-       this.el.parentEl.components &&
-       this.el.parentEl.components['selection-folder']) {
-       g = this.el.parentEl.components['selection-folder'].data.folder
-     }
+     var g = findParentGUI(this.el);
      g.addColor(this.data, 'color').name(this.data.name)
    }
 });
 
-AFRAME.registerComponent('selection-folder', {
+AFRAME.registerComponent('number-selector', {
    schema: {
-     name: { default: 'folder', type: 'string' },
-     open: { default: true, type: 'boolean' }
+     number: { default: '0', type: 'number' },
+     name: { default: 'number', type: 'string' },
+     min: { default: null, type: 'number' },
+     max: { default: null, type: 'number' },
+     step: { default: null, type: 'number' }
    },
    init: function () {
-     console.log('selection-folder',datGUI);
+     console.log('number-selector',datGUI);
+     console.log(this.data)
      if (datGUI == undefined) {
         datGUI = new dat.GUI();       
      }
-     this.data.folder = datGUI.addFolder(this.data.name)
-     if (this.data.open) {
-       this.data.folder.open();
-     }
+     var g = findParentGUI(this.el);
+
+     var s = g.add(this.data, 'number').name(this.data.name);
+
+     var that = this;
+     ['min','max','step'].forEach(function(o) {
+       if (that.data[o] != null) {
+         s = s[o](that.data[o]);
+       }
+     });
    }
 });
+
+
