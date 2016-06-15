f6432cbc396e4e93a4c7bbc2a66c872c1f2d94f6
Andy Gill <andygill@ku.edu>
2016-06-15 16:48:31 -0500

    Adding numbers and basic behaviors

--- examples/js/aframe-my-test.js
+++ examples/js/aframe-my-test.js
@@ -15,6 +15,7 @@ function findParentGUI(el) {
   }
 }
 
+
 AFRAME.registerComponent('selection-folder', {
    schema: {
      name: { default: 'folder', type: 'string' },
@@ -39,17 +40,29 @@ AFRAME.registerComponent('selection-folder', {
 
 AFRAME.registerComponent('color-selector', {
    schema: {
-     color: { default: '#FFF', type: 'string' },
+     value: { default: '#FFF', type: 'string' },
      name: { default: 'color', type: 'string' }
    },
    init: function () {
      console.log('color-selector',datGUI);
      var g = findParentGUI(this.el);
      var el = this.el;
-     g.addColor(this.data, 'color').name(this.data.name).onChange(function(value) {
-       el.setAttribute('color-selector','color',value);
-     });
+     function change(value) {
+       if (el.tagName == "A-COLOR-SELECTOR") { 
+         el.setAttribute('value',value);
+       } else {
+         el.setAttribute('color-selector','value',value);
+       }
+     }
+     g.addColor(this.data, 'value').name(this.data.name).onChange(change);
    }
+});
+
+AFRAME.registerPrimitive('a-color-selector',{
+  mappings: {
+    name:  'color-selector.name',
+    value: 'color-selector.value'
+  }
 });
 
 AFRAME.registerComponent('number-selector', {
@@ -70,7 +83,11 @@ AFRAME.registerComponent('number-selector', {
      var el = this.el;
 
      var s = g.add(this.data, 'number').name(this.data.name).onChange(function(value) {
-       el.setAttribute('number-selector','number',value);
+       if (el.tagName == "A-NUMBER-SELECTOR") { 
+         el.setAttribute('number',value);
+       } else {
+         el.setAttribute('number-selector','number',value);
+       }
      });
 
      var that = this;
@@ -82,4 +99,60 @@ AFRAME.registerComponent('number-selector', {
    }
 });
 
+AFRAME.registerPrimitive('a-number-selector',{
+  defaultComponents: {
+    "number-selector": { }
+  },
+  mappings: {
+    number: 'number-selector.number',
+    name: 'number-selector.name',
+    min: 'number-selector.min',
+    max: 'number-selector.max',
+    step: 'number-selector.step'
+  }
+});
 
+
+AFRAME.registerComponent('frp', {
+  schema: {
+    attribute: { default: null, type: 'string' },
+    behavior:  { default: null, type: 'string' }
+  },
+   init: function () {
+     console.log('frp',this.data);
+   },
+   tick: function(o) {
+     var self = this;
+     var target  = this.el.parentEl;
+     var oldAttr = target.getAttribute(this.data.attribute);
+     var env =
+       { vec3: function(x,y,z) { return {x:x,y:y,z:z}; },
+         "$": function(o) { 
+            var el = document.getElementById(o);
+            return 0;
+        }
+       };
+     var res = undefined;
+     try { 
+       with (env) { res = eval(this.data.behavior); }
+     } 
+     catch(ex) {
+       console.log(ex);
+     }
+//     console.log("frp",res);
+     target.setAttribute("rotation",{x:o/10,y:0,z:0});
+       if (this.done == undefined) {
+       console.log("frp",this,target);
+       this.done = true;
+     }
+
+//     console.log('frp-tick',o);
+   }
+ });
+
+ AFRAME.registerPrimitive('a-frp',{
+   mappings: {
+     attribute: 'frp.attribute',
+     behavior:  'frp.behavior'
+   }
+ });
