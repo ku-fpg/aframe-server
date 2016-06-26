960db233079334de00eee7616505d57ca725857b
Andy Gill <andygill@ku.edu>
2016-06-26 18:22:16 -0500

    Position refactor

--- src/Text/AFrame/Geometry.hs
+++ src/Text/AFrame/Geometry.hs
@@ -16,11 +16,15 @@ import Debug.Trace
 -- When using Geometry, use (position p <> G.rotation YXZ r <>  scale s)
 -- aka scale, then rotate, then position.
 
-newtype Geometry a = Geometry (Position a -> Position a)
+newtype Geometry a = Geometry (Position' a -> Position' a)
 
 type Position a    = (a,a,a) -- in units, x,y,z
 
-type Rotation a    = (a,a,a) -- in degrees, x,y,z
+newtype Position' a  = Position (a,a,a) -- in units, x,y,z
+        deriving Show
+
+newtype Rotation a = Rotation (a,a,a) -- in degrees, x,y,z
+        deriving Show
 
 newtype Vector a   = Vector (a,a,a) -- in units, x,y,z
         deriving Show
@@ -28,7 +32,8 @@ newtype Vector a   = Vector (a,a,a) -- in units, x,y,z
 newtype Normal a   = Normal (a,a,a) -- unit length, x,y,z
         deriving Show
         
-type Scale a       = (a,a,a) -- ratio, x,y,z
+newtype Scale a    = Scale (a,a,a) -- ratio, x,y,z
+        deriving Show
 
 data Order = XYZ | XZY | YXZ | YZX | ZXY | ZYX
 
@@ -45,13 +50,14 @@ instance Monoid (Geometry a) where
 --instance Geometric (Position a) where
 --  run (Geometry f) = f
 
-run = runPosition
+run :: Geometry a -> Position' a -> Position' a
+run (Geometry f) = f
 
 runPosition :: Geometry a -> Position a -> Position a
-runPosition (Geometry f) = f
+runPosition (Geometry f) p = let Position p' = f (Position p) in p'
 
-position :: Num a => Position a -> Geometry a
-position (xd,yd,zd) = Geometry $ \ (x,y,z) -> (x + xd,y + yd,z + zd)
+position :: Num a => (a,a,a) -> Geometry a
+position (xd,yd,zd) = Geometry $ \ (Position (x,y,z)) -> Position (x + xd,y + yd,z + zd)
 
 -- 'rotation' is in degrees
 
@@ -69,8 +75,8 @@ position (xd,yd,zd) = Geometry $ \ (x,y,z) -> (x + xd,y + yd,z + zd)
 -- Quote: .. about the axes of the rotating coordinate system, 
 -- which changes its orientation after each elemental rotation (intrinsic rotations).
 
-rotation :: Floating a => Order -> Rotation a -> Geometry a
-rotation order (xd,yd,zd) = Geometry $ case order of
+rotation :: Floating a => Order -> (a,a,a) -> Geometry a
+rotation order (xd,yd,zd) = Geometry $ runPosition $ case order of
       YXZ -> rotY . rotX . rotZ 
        -- This seems reversed to me, this is because
        -- aframe/THREE uses intrinsic, and have extrinsic rotations.
@@ -78,6 +84,9 @@ rotation order (xd,yd,zd) = Geometry $ case order of
        -- equivalent and vice-versa by reversing the order of elemental rotations.
        -- From: http://danceswithcode.net/engineeringnotes/rotations_in_3d/rotations_in_3d_part1.html
   where
+   runPosition f (Position p) = Position $ f p
+
+
    rotX (x,y,z) = (x, y',z') where (y',z') = rot xd    (y,z)
    rotY (x,y,z) = (x',y ,z') where (x',z') = rot (-yd) (x,z)
    rotZ (x,y,z) = (x',y',z ) where (x',y') = rot zd    (x,y)
@@ -87,13 +96,13 @@ rotation order (xd,yd,zd) = Geometry $ case order of
                  )
      where r = d / 180 * pi
 
-scale :: Num a => Scale a -> Geometry a
-scale (xd,yd,zd) = Geometry $ \ (x0,y0,z0) -> (xd * x0, yd * y0, zd * z0)
+scale :: Num a => (a,a,a) -> Geometry a
+scale (xd,yd,zd) = Geometry $ \ (Position (x0,y0,z0)) -> Position (xd * x0, yd * y0, zd * z0)
 
 
 -- assuming we are looking at the -ve side of the z axis.
 perspective :: Fractional a => Geometry a
-perspective = Geometry $ \ (x,y,z) -> (x/(-z),y/(-z),z)
+perspective = Geometry $ \ (Position (x,y,z)) -> Position (x/(-z),y/(-z),z)
 
 --------------------------------------------------------------------------------------------------------
 -- Misc linear alg. operators
@@ -101,17 +110,17 @@ perspective = Geometry $ \ (x,y,z) -> (x/(-z),y/(-z),z)
 size :: Floating a => Vector a -> a
 size (Vector (x,y,z)) = sqrt (x^2 + y^2 + z^2)
 
-to :: Num a => Position a -> Position a -> Vector a
-to (x0,y0,z0) (x1,y1,z1) = Vector (x1 - x0,y1 - y0,z1 - z0)
+to :: Num a => Position' a -> Position' a -> Vector a
+to (Position (x0,y0,z0)) (Position (x1,y1,z1)) = Vector (x1 - x0,y1 - y0,z1 - z0)
 
 normalize :: Floating a => Vector a -> Normal a
 normalize (Vector (x,y,z)) = (Normal (x/d,y/d,z/d))
-  where d = size $ (0,0,0) `to` (x,y,z)
-
+  where d = size $ Position (0,0,0) `to` Position (x,y,z)
 
 runNormal :: Floating a => Geometry a -> Normal a -> Normal a
-runNormal g (Normal p) = normalize $ Vector $ run (position (-x,-y,-z) <> g) $ p
-  where (x,y,z) = run g (0,0,0)
+runNormal g (Normal p) = normalize $ Vector $ p'
+  where Position (x,y,z) = run                         g  $ Position (0,0,0)
+        Position p'      = run (position (-x,-y,-z) <> g) $ Position p
   
 crossProduct :: Num a => Vector a -> Vector a -> Vector a
 crossProduct (Vector (bx,by,bz)) (Vector (cx,cy,cz)) = Vector (ax,ay,az)
@@ -124,33 +133,36 @@ dotProduct :: Num a => Vector a -> Vector a -> a
 dotProduct (Vector (bx,by,bz)) (Vector (cx,cy,cz)) = bx*cx + by*cy + bz*cz
 
 ------------------------------------------------------------------------------------------------
-
 class Polygon p where
-  path  :: p a -> [Position a]
-  mapPosition :: (Position a -> Position a) -> p a -> p a
+  path  :: p a -> [Position' a]
+  mapPosition :: Geometry a -> p a -> p a       -- replace with superclass
 
 -- Assumes a planer convext polygon. Like a Triangle or Quad.
 tessellation :: Polygon p => p a -> [Triangle a]
-tessellation ps = [ Triangle p1 p2 p3 | (p1:p2:p3:_) <- take (length (path ps) - 2) $ cycle $ iterate tail $ path $ ps ]
+tessellation ps = 
+        [ Triangle p1 p2 p3 
+        | (p1:p2:p3:_) <- take (length (path ps) - 2) $ cycle $ iterate tail $ path $ ps 
+        ]
+
 
 -- Depth sorting, where lower z-axis numbers mean behind 
-data Quad a = Quad (Position a) (Position a) (Position a) (Position a)
+data Quad a = Quad (Position' a) (Position' a) (Position' a) (Position' a)
   deriving Show
    -- must have the same normal.
 
 instance Polygon Quad where
   path (Quad p0 p1 p2 p3) = [p0,p1,p2,p3]
-  mapPosition f (Quad p0 p1 p2 p3) = Quad (f p0) 
-                                          (f p1)
-                                          (f p2)
-                                          (f p3)
+  mapPosition f (Quad p0 p1 p2 p3) = Quad (run f p0) 
+                                          (run f p1)
+                                          (run f p2)
+                                          (run f p3)
 
-data Triangle a = Triangle (Position a) (Position a) (Position a) 
+data Triangle a = Triangle (Position' a) (Position' a) (Position' a) 
   deriving Show
 
 instance Polygon Triangle where
   path (Triangle p0 p1 p2) = [p0,p1,p2]
-  mapPosition f (Triangle p1 p2 p3) = Triangle (f p1) (f p2) (f p3)
+  mapPosition f (Triangle p1 p2 p3) = Triangle (run f p1) (run f p2) (run f p3)
   
 data Stacking = Behind | InFront | Disjoint | Indeterminate
   deriving (Show,Eq)
@@ -178,20 +190,21 @@ flattenPolygon :: (Show a, Polygon p, RealFloat a) => p a -> Geometry a
 flattenPolygon ps = normalRotation (normalize $ crossProduct p0p1 p0p2) (Normal (0,0,1))
                  <> position (-x0*1,-y0*1,-z0)
   where 
-    (p0@(x0,y0,z0):p1@(x1,y1,z1):p2@(x2,y2,z2):_) = path ps
+    (p0@(Position (x0,y0,z0)):p1@(Position (x1,y1,z1)):p2@(Position (x2,y2,z2)):_) = path ps
     p0p1 = p0 `to` p1
     p0p2 = p0 `to` p2
+
       
 comparePolygon :: (Show a, Ord a, RealFloat a,Polygon p) => p a -> p a -> Stacking
 comparePolygon pa pb
     -- Is it trivial in the z axis?
-    | minimum [ z | (x,y,z) <- as ] > maximum [ z | (x,y,z) <- bs] = InFront
-    | maximum [ z | (x,y,z) <- as ] < minimum [ z | (x,y,z) <- bs] = Behind
+    | minimum [ z | Position (x,y,z) <- as ] > maximum [ z | Position (x,y,z) <- bs] = InFront
+    | maximum [ z | Position (x,y,z) <- as ] < minimum [ z | Position (x,y,z) <- bs] = Behind
     -- Are the seperate on the x or y axis?
-    | minimum [ x | (x,y,z) <- as ] > maximum [ x | (x,y,z) <- bs] = Disjoint
-    | maximum [ x | (x,y,z) <- as ] < minimum [ x | (x,y,z) <- bs] = Disjoint
-    | minimum [ y | (x,y,z) <- as ] > maximum [ y | (x,y,z) <- bs] = Disjoint
-    | maximum [ y | (x,y,z) <- as ] < minimum [ y | (x,y,z) <- bs] = Disjoint
+    | minimum [ x | Position (x,y,z) <- as ] > maximum [ x | Position (x,y,z) <- bs] = Disjoint
+    | maximum [ x | Position (x,y,z) <- as ] < minimum [ x | Position (x,y,z) <- bs] = Disjoint
+    | minimum [ y | Position (x,y,z) <- as ] > maximum [ y | Position (x,y,z) <- bs] = Disjoint
+    | maximum [ y | Position (x,y,z) <- as ] < minimum [ y | Position (x,y,z) <- bs] = Disjoint
 
     -- Otherwise, give up
     | otherwise = Indeterminate
@@ -212,10 +225,11 @@ comparePolygon pa pb
 eps :: Fractional a => a
 eps = 0.00001
 
+
 intersectQuad :: (Ord a, Fractional a) => Quad a -> (a,a) -> Maybe a
 intersectQuad (Quad p0 p1 p2 p3) (x,y) = 
     intersectTriangle (Triangle p0 p1 p2) (x,y) <|> intersectTriangle (Triangle p2 p3 p0) (x,y)
-  
+
 -- takes a triangle, and returns the value along the z-axis from the xy-plane, if any.
 intersectTriangle :: (Ord a, Fractional a) => Triangle a -> (a,a) -> Maybe a
 intersectTriangle (Triangle p0 p1 p2) (x,y) 
@@ -225,7 +239,7 @@ intersectTriangle (Triangle p0 p1 p2) (x,y)
     | otherwise          = Just (-t) -- -ve because the z-axis, -ve is away, and +ve is closer.
   where
     dir  = Vector (0,0,-1)
-    orig = (x,y,0)
+    orig = Position (x,y,0)
       
     p0p1 = p0 `to` p1
     p0p2 = p0 `to` p2
@@ -242,7 +256,6 @@ intersectTriangle (Triangle p0 p1 p2) (x,y)
     v = dotProduct dir qvec * invDet
     
     t = dotProduct p0p2 qvec * invDet
-
 ------------------------------------------------------------------------------------
 -- Testing
 
@@ -268,32 +281,33 @@ splat f = unlines
 test = putStrLn $ splat $ fmap (fmap draw) $ intersectTriangle t1
 
 t1 :: Triangle Double
-t1 = Triangle (0,-2,-2) (1,1,5)  (-1.2,1,-5) 
+t1 = Triangle (Position (0,-2,-2)) (Position (1,1,5)) (Position (-1.2,1,-5) )
 
 test2 = putStrLn $ splat $ fmap (fmap draw) $ intersectQuad q1
 
 test3 = putStrLn $ splat $ fmap (fmap draw) $ intersectQuad 
-   $ mapPosition (run (perspective <> position (0,0,-2) <> rotation YXZ (-70,0,0)))
+   $ mapPosition ((perspective <> position (0,0,-2) <> rotation YXZ (-70,0,0)))
    $ plane 2 2
 
 q1 :: Quad Double
-q1 = Quad (-1,-1,-1) (1,-1,-1) (1,1,-1) (-1,1,-4) 
+q1 = Quad (Position (-1,-1,-1)) (Position (1,-1,-1)) (Position (1,1,-1)) (Position (-1,1,-4))
 
+-- Quad goes counter-clock
 plane :: Fractional a => a -> a -> Quad a
-plane w h = Quad (-w/2,h/2,0) (-w/2,-h/2,0) (w/2,-h/2,0) (w/2,h/2,0) 
+plane w h = Quad (Position (-w/2,h/2,0)) (Position (-w/2,-h/2,0)) (Position (w/2,-h/2,0)) (Position (w/2,h/2,0))
 
 test4 n = putStrLn $ splat $ fmap (fmap draw) $ intersectQuad $ quad'
   where
-    quad' = mapPosition (run (flattenPolygon quad)) quad
+    quad' = mapPosition ( (flattenPolygon quad)) quad
 
-    quad = mapPosition (run (position (0,0,0) <> rotation YXZ (n,10,-10)))
+    quad = mapPosition ( (position (0,0,0) <> rotation YXZ (n,10,-10)))
           $ plane 2 2
   
   
 test5 = comparePolygon q1 q2
   where        
-    q1 = mapPosition (run (position (0,0,0) <> rotation YXZ (80,10,-10)))
+    q1 = mapPosition ((position (0,0,0) <> rotation YXZ (80,10,-10)))
        $ plane 2 2
 
-    q2 = mapPosition (run (position (5,5,0) <> rotation YXZ (80,10,-10)))
+    q2 = mapPosition ( (position (5,5,0) <> rotation YXZ (80,10,-10)))
        $ plane 2 2
